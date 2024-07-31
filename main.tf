24da6c131fe95c85f36cbd330f3333c4887277db
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-07-30 20:20:35 -0700

    Provision Web Server VM

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ terraform {
       version = "~> 3.0.2"
     }
   }
+  backend "azurerm" {
+    resource_group_name   = "resume"
+    storage_account_name  = "qliustorage" # Ensure this follows Azure naming conventions
+    container_name        = "terraformstate"
+    key                   = "terraform.tfstate"
+  }
 
   required_version = ">= 1.1.0"
 }
@@ -14,4 +20,78 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "resume" {
+  name     = var.resource_group_name
+  location = var.location
 
+  tags = {
+    Environment = "developing IaC for resume website"
+    Team        = "Steven"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "main"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resume.location
+  resource_group_name = azurerm_resource_group.resume.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "main" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.resume.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a public IP
+resource "azurerm_public_ip" "resume_ip" {
+  name                = "resume-ip"
+  location            = azurerm_resource_group.resume.location
+  resource_group_name = azurerm_resource_group.resume.name
+  allocation_method   = "Dynamic"
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "resume_nic" {
+  name                = "resume-nic"
+  location            = azurerm_resource_group.resume.location
+  resource_group_name = azurerm_resource_group.resume.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.resume_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "resume_web" {
+  name                = "resume-web"
+  resource_group_name = azurerm_resource_group.resume.name
+  location            = azurerm_resource_group.resume.location
+  size                = "Standard_F2"
+  admin_username      = "domainadmin"
+  network_interface_ids = [
+    azurerm_network_interface.resume_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "domainadmin"
+    public_key = file(".ssh/id_rsa.pub") # Ensure this path is correct
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,8 @@
+output "resource_group_id" {
+  value = azurerm_resource_group.resume.id
+}
+
+output "public_ip_address" {
+  description = "The public IP address of the VM"
+  value       = azurerm_public_ip.resume_ip.ip_address
+}
