4ad93146867a49cd9c1253b90dccf9b68a62a36b
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-08-06 21:00:59 -0700

    cancel subnet change

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "azurerm_subnet" "main" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.resume.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Create a public IP
