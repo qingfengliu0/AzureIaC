57919987e6a2bf5dc347a1eebb7702afc0c0aed4
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-08-09 21:44:28 -0700

    remove the custom cdn for now

--- main.tf
+++ main.tf
@@ -161,16 +161,16 @@ provider "cloudflare" {
 resource "cloudflare_record" "cdn-cname-record" {
   zone_id = var.cloudflare_zone_id
   name    = var.dns_name
-  value   = azurerm_cdn_endpoint.resume-cdn-endpoint-qliu.name
+  value   = azurerm_cdn_endpoint.resume-cdn-endpoint-qliu.fqdn
   type    = "CNAME"
   ttl     = 300
 }
 
 # Add a Custom Domain to the CDN Endpoint
-resource "azurerm_cdn_endpoint_custom_domain" "qliu-cdn-domain" {
-  name                = "qliu-cdn-domain"
-  cdn_endpoint_id       = azurerm_cdn_endpoint.resume-cdn-endpoint-qliu.id
-  host_name            = var.dns_name # Your custom domain
+#resource "azurerm_cdn_endpoint_custom_domain" "qliu-cdn-domain" {
+#  name                = "qliu-cdn-domain"
+#  cdn_endpoint_id       = azurerm_cdn_endpoint.resume-cdn-endpoint-qliu.id
+#  host_name            = var.dns_name # Your custom domain
 
   # Azure CDN requires the custom domain to already have a CNAME entry pointing to the CDN endpoint
-}
+#}
