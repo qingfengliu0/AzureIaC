ebc14798fd8661d4a139aa1dd9c776e3281aee68
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-08-11 16:14:27 -0700

    enable the http traffic on blob storage

--- main.tf
+++ main.tf
@@ -41,6 +41,9 @@ resource "azurerm_storage_account" "resumewebstorage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+  # Disable secure transfer required
+  enable_https_traffic_only = false
+  
   static_website {
     index_document     = "index.html"
     error_404_document = "404.html"
