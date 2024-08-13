3951f10a568c88a44f96d4295eee183147d9678e
qingfengliu <qingfengliu@zohomail.com>
2024-08-13 16:48:42 -0700

    added required blocks

--- main.tf
+++ main.tf
@@ -202,6 +202,19 @@ resource "azurerm_cosmosdb_account" "qliudb" {
   resource_group_name = azurerm_resource_group.resume.name
   offer_type          = "Standard"
   kind                = "MongoDB"
+  
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level = "Strong"
+  }
+
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
 }
 #Create the Database 
 resource "azurerm_cosmosdb_mongo_database" "visitor-counter-db" {
