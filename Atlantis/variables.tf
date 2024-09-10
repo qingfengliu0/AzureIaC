5b7174485342e7391cdcd9b5f3982cc65a6c3430
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-09-10 15:01:47 -0700

    remove tenant id as sensitive information

--- Atlantis/variables.tf
+++ Atlantis/variables.tf
@@ -11,10 +11,3 @@ variable "admin_username" {
   default     = "domainadmin"
 }
 
-variable "tenant_id" {
-  description = "tenant id"
-  type        = string
-  sensitive   = true
-  default     = "5caffd0a-25ae-4d12-a6e2-6646216cf62e"
-}
-
