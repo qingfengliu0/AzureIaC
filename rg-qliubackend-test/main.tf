838f08efd5c78bb831a60f10d0ff2bb89e9990a1
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-09-11 16:30:55 -0700

    delted indenpently created groups

--- rg-qliubackend-test/main.tf
+++ rg-qliubackend-test/main.tf
@@ -24,50 +24,50 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg-qliubackend-test" {
-  name     = "rg-qliubackend-test"
-  location = "westus2"
+# resource "azurerm_resource_group" "rg-qliubackend-test" {
+#   name     = "rg-qliubackend-test"
+#   location = "westus2"
 
-  tags = {
-    Environment = "Development"
-    Description = "Resource group for storing backend data for resume.qliu.ca"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#     Description = "Resource group for storing backend data for resume.qliu.ca"
+#   }
+# }
 
-# Azure Cosmos DB account
-resource "azurerm_cosmosdb_account" "cosmos-qliubackend-test" {
-  name                = "cosmos-qliubackend-test"
-  location            = azurerm_resource_group.rg-qliubackend-test.location
-  resource_group_name = azurerm_resource_group.rg-qliubackend-test.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
+# # Azure Cosmos DB account
+# resource "azurerm_cosmosdb_account" "cosmos-qliubackend-test" {
+#   name                = "cosmos-qliubackend-test"
+#   location            = azurerm_resource_group.rg-qliubackend-test.location
+#   resource_group_name = azurerm_resource_group.rg-qliubackend-test.name
+#   offer_type          = "Standard"
+#   kind                = "GlobalDocumentDB"
 
-  geo_location {
-    location          = "westus2"
-    failover_priority = 0
-  }
+#   geo_location {
+#     location          = "westus2"
+#     failover_priority = 0
+#   }
 
-  consistency_policy {
-    consistency_level = "Strong"
-  }
-}
+#   consistency_policy {
+#     consistency_level = "Strong"
+#   }
+# }
 
-# Azure Database
-resource "azurerm_cosmosdb_sql_database" "cosmos-sql-db-test" {
-  name                = "cosmos-sql-db-test"
-  resource_group_name = azurerm_resource_group.rg-qliubackend-test.name
-  account_name        = azurerm_cosmosdb_account.cosmos-qliubackend-test.name
-  throughput          = 400
-}
+# # Azure Database
+# resource "azurerm_cosmosdb_sql_database" "cosmos-sql-db-test" {
+#   name                = "cosmos-sql-db-test"
+#   resource_group_name = azurerm_resource_group.rg-qliubackend-test.name
+#   account_name        = azurerm_cosmosdb_account.cosmos-qliubackend-test.name
+#   throughput          = 400
+# }
 
-# Azure Container
-resource "azurerm_cosmosdb_sql_container" "cosmos-sql-container-test" {
-  name                  = "cosmos-sql-container-test"
-  resource_group_name   = azurerm_resource_group.rg-qliubackend-test.name
-  account_name          = azurerm_cosmosdb_account.cosmos-qliubackend-test.name
-  database_name         = azurerm_cosmosdb_sql_database.cosmos-sql-db-test.name
-  partition_key_paths   = ["/id"]
-  partition_key_version = 1
-  throughput            = 400
+# # Azure Container
+# resource "azurerm_cosmosdb_sql_container" "cosmos-sql-container-test" {
+#   name                  = "cosmos-sql-container-test"
+#   resource_group_name   = azurerm_resource_group.rg-qliubackend-test.name
+#   account_name          = azurerm_cosmosdb_account.cosmos-qliubackend-test.name
+#   database_name         = azurerm_cosmosdb_sql_database.cosmos-sql-db-test.name
+#   partition_key_paths   = ["/id"]
+#   partition_key_version = 1
+#   throughput            = 400
 
-}
+# }
