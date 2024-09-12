4eed1eff3d51171af7856dd547db9b6dfe75bc78
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-09-11 18:53:09 -0700

    explicit declare the provider

--- modules/rg-qliufrontend-test/main.tf
+++ modules/rg-qliufrontend-test/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.1.0"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "4.35.0"
+    }
+    
+  }
+}
 
 provider "cloudflare" {
   email   = var.cloudflare_email
