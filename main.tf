635319c60f09c61c6fd60c06650cc5cdefd83fee
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-09-11 18:55:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,4 +25,6 @@ provider "azurerm" {
 
 module "rg-qliufrontend-test" {
   source = "./modules/rg-qliufrontend-test"
+  admin_password = var.admin_password
+  cloudflare_api_key = var.cloudflare_api_key
 }
