551f89381920187806aeb1bfa0c96e1cabb1e995
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-09-11 18:20:48 -0700

    change version

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 4.1.0"
     }
     cloudflare = {
       source = "cloudflare/cloudflare"
