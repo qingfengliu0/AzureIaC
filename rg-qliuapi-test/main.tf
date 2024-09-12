b6bfffe8ab5ea8dc58611d7da549234619376c7a
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-09-11 17:06:51 -0700

    remove the rest

--- rg-qliuapi-test/main.tf
+++ rg-qliuapi-test/main.tf
@@ -1,27 +1,27 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.1.0"
-    }
-    cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "4.35.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name   = "atlantis-resource-group"
-    storage_account_name  = "qliuatlantisstorage" # Ensure this follows Azure naming conventions
-    container_name        = "qliutfstatecontainer"
-    key                   = "rg-qliuapi-test.tfstate"
-  }
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 4.1.0"
+#     }
+#     cloudflare = {
+#       source = "cloudflare/cloudflare"
+#       version = "4.35.0"
+#     }
+#   }
+#   backend "azurerm" {
+#     resource_group_name   = "atlantis-resource-group"
+#     storage_account_name  = "qliuatlantisstorage" # Ensure this follows Azure naming conventions
+#     container_name        = "qliutfstatecontainer"
+#     key                   = "rg-qliuapi-test.tfstate"
+#   }
 
-  required_version = ">= 1.1.0"
-}
+#   required_version = ">= 1.1.0"
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
 # resource "azurerm_resource_group" "rg-qliuapi-test" {
 #   name     = "rg-qliuapi-test"
