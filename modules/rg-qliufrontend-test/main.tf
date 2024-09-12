d537bb0ee2d4de26e667355fca585a0f6e5ad3a4
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-09-12 15:13:18 -0700

    add feature

--- modules/rg-qliufrontend-test/main.tf
+++ modules/rg-qliufrontend-test/main.tf
@@ -12,6 +12,14 @@ terraform {
   }
 }
 
+provider "azurerm"{
+  features{
+        resource_group {
+            prevent_deletion_if_contains_resources = false
+        }
+  }
+}
+
 provider "cloudflare" {
   email   = var.cloudflare_email
   api_key = var.cloudflare_api_key
@@ -20,7 +28,6 @@ provider "cloudflare" {
 resource "azurerm_resource_group" "resume" {
   name     = var.resource_group_name
   location = var.location
-
   tags = {
     Environment = "developing IaC for resume website"
     Team        = "Steven"
