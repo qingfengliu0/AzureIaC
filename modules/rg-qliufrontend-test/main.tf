5bb4392d2031c29a7fb76f09f4269e21f8e94312
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-09-12 17:02:58 -0700

    correct name reference

--- modules/rg-qliufrontend-test/main.tf
+++ modules/rg-qliufrontend-test/main.tf
@@ -8,105 +8,109 @@ terraform {
       source = "cloudflare/cloudflare"
       version = "4.35.0"
     }
-    
   }
 }
+
 provider "cloudflare" {
   email   = var.cloudflare_email
   api_key = var.cloudflare_api_key
+}
+
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "rg-qliufrontend-test" {
   name     = "rg-qliufrontend-test"
   location = var.location
   tags = {
-    Environment = "developing IaC for resume website"
+    Environment = "developing IaC for rg-qliufrontend-test website"
     Team        = "Steven"
   }
 }
-#Create a Blob Storage for holding the static code
+
+# Create a Blob Storage for holding the static code
 resource "azurerm_storage_account" "st-qliufrontend-test" {
-  name                     = "resumewebstorage"
-  resource_group_name      = azurerm_resource_group.resume.name
-  location                 = azurerm_resource_group.resume.location
+  name                     = "stqliufrontendtest" # Ensure this name is globally unique
+  resource_group_name      = azurerm_resource_group.rg-qliufrontend-test.name
+  location                 = azurerm_resource_group.rg-qliufrontend-test.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
+
   static_website {
     index_document     = "index.html"
     error_404_document = "404.html"
   }
 }
-#Create a CDN Profile
+
+# Create a CDN Profile
 resource "azurerm_cdn_profile" "cdnp-qliufrontend-test" {
   name                = "cdnp-qliufrontend-test"
   location            = "eastus"
-  resource_group_name = azurerm_resource_group.resume.name
+  resource_group_name = azurerm_resource_group.rg-qliufrontend-test.name
   sku                 = "Standard_Microsoft"
 }
 
-#Create a CDN Endpoint, origin is the web storage endpoint
+# Create a CDN Endpoint, origin is the web storage endpoint
 resource "azurerm_cdn_endpoint" "cdne-qliufrontend-test" {
   name                = "cdne-qliufrontend-test"
-  resource_group_name = azurerm_resource_group.resume.name
-  profile_name        = azurerm_cdn_profile.resume-cdn-profile.name
+  resource_group_name = azurerm_resource_group.rg-qliufrontend-test.name
+  profile_name        = azurerm_cdn_profile.cdnp-qliufrontend-test.name # Fixed reference to CDN profile
   location            = "eastus"
-  origin_path         = "/public"
+  origin_path         = "/public" # Ensure this is correct for your static website
+
   origin {
     name      = "storage-origin"
-    host_name = azurerm_storage_account.resumewebstorage.primary_web_host
+    host_name = azurerm_storage_account.st-qliufrontend-test.primary_web_host
     http_port = 80
     https_port = 443
-  
   }
-  origin_host_header = azurerm_storage_account.resumewebstorage.primary_web_host
+  
+  origin_host_header = azurerm_storage_account.st-qliufrontend-test.primary_web_host
 
-  is_http_allowed = true
+  is_http_allowed  = true
   is_https_allowed = true
 
   delivery_rule {
-    name = "redirect2https"
+    name  = "redirect2https"
     order = 1
 
     request_uri_condition {
-      operator  = "Equal"
+      operator    = "Equal"
       match_values = ["/"]
     }
 
     request_scheme_condition {
-      operator     = "Equal"
+      operator    = "Equal"
       match_values = ["HTTP"]
     }
 
     url_redirect_action {
       redirect_type = "Found"
-      protocol = "Https"
-      hostname = azurerm_storage_account.resumewebstorage.primary_web_host
+      protocol      = "Https"
+      hostname      = azurerm_storage_account.st-qliufrontend-test.primary_web_host
     }
   }
-  
 }
 
-# Define the DNS record
+# Define the DNS record in Cloudflare
 resource "cloudflare_record" "dns-qliufrontend-test" {
   zone_id = var.cloudflare_zone_id
   name    = var.dns_name
-  value   = azurerm_cdn_endpoint.resume-cdn-endpoint-qliu.fqdn
+  value   = azurerm_cdn_endpoint.cdne-qliufrontend-test.hostname # Use .hostname instead of .fqdn
   type    = "CNAME"
   ttl     = 300
 }
 
 # Add a Custom Domain to the CDN Endpoint
 resource "azurerm_cdn_endpoint_custom_domain" "domain-qliufrontend-test" {
-  name                = "qliu-cdn-domain"
-  cdn_endpoint_id     = azurerm_cdn_endpoint.resume-cdn-endpoint-qliu.id
-  host_name           = var.dns_name # Your custom domain
+  name            = "qliu-cdn-domain"
+  cdn_endpoint_id = azurerm_cdn_endpoint.cdne-qliufrontend-test.id
+  host_name       = var.dns_name # Your custom domain
+
   cdn_managed_https {
     certificate_type = "Dedicated"
-    protocol_type = "ServerNameIndication"
-    tls_version = "TLS12"
+    protocol_type    = "ServerNameIndication"
+    tls_version      = "TLS12"
   }
-
-
-  # Azure CDN requires the custom domain to already have a CNAME entry pointing to the CDN endpoint
 }
