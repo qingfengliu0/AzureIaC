55d1adfa4db6938a755f3975fc99aa226029d73e
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2024-11-03 17:56:43 -0800

    lower case

--- modules/rg-qliufrontend-test/main.tf
+++ modules/rg-qliufrontend-test/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_storage_account" "st-qliufrontend-test" {
   location                 = azurerm_resource_group.rg-qliufrontend-test.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  https_traffic_only_enabled = False 
+  https_traffic_only_enabled = false 
   static_website {
     index_document     = "index.html"
     error_404_document = "404.html"
