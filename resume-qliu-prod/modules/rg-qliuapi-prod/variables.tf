892032a1af0490a050dd8908eb36c4d100cca3d8
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2025-06-30 21:08:32 -0700

    adding production api

---
+++ resume-qliu-prod/modules/rg-qliuapi-prod/variables.tf
@@ -0,0 +1,22 @@
+variable "db_connectionstring" {
+  description = "connection string to the comos db account" 
+  type        = string
+  sensitive   = true
+}
+
+variable "location"{
+  type = string
+  default = "westus2"
+}
+
+variable "slack_webhook"{
+  description = "slack webhook url for alert notification"
+  type        = string
+  sensitive   = true
+}
+
+variable "pagerduty_webhook"{
+  description = "pager webhook url for alert notification"
+  type        = string
+  sensitive   = true
+}
