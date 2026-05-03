a3a262b858a6e6db9934182ad909f2680b04e2e5
qingfengliu <66491564+qingfengliu0@users.noreply.github.com>
2026-05-02 19:11:20 -0700

    remove the reditection

--- resume-qliu-test/modules/rg-qliufrontend-test/main.tf
+++ resume-qliu-test/modules/rg-qliufrontend-test/main.tf
@@ -110,32 +110,32 @@ resource "cloudflare_record" "dns-qliufrontend-test" {
   proxied = true
 }
 
-resource "cloudflare_ruleset" "redirect_root_to_public_index" {
-  zone_id     = var.cloudflare_zone_id
-  name        = "redirects"
-  description = "Redirect root requests to the Azure static website public index"
-  kind        = "zone"
-  phase       = "http_request_dynamic_redirect"
+# resource "cloudflare_ruleset" "redirect_root_to_public_index" {
+#   zone_id     = var.cloudflare_zone_id
+#   name        = "redirects"
+#   description = "Redirect root requests to the Azure static website public index"
+#   kind        = "zone"
+#   phase       = "http_request_dynamic_redirect"
 
-  rules {
-    ref         = "redirect_root_to_public_index"
-    description = "Redirect root URL to Azure static website /public/index.html"
-    expression  = "(http.host eq \"${var.dns_name}\" and http.request.uri.path eq \"/\")"
-    action      = "redirect"
+#   rules {
+#     ref         = "redirect_root_to_public_index"
+#     description = "Redirect root URL to Azure static website /public/index.html"
+#     expression  = "(http.host eq \"${var.dns_name}\" and http.request.uri.path eq \"/\")"
+#     action      = "redirect"
 
-    action_parameters {
-      from_value {
-        status_code = 301
+#     action_parameters {
+#       from_value {
+#         status_code = 301
 
-        target_url {
-          value = "https://${azurerm_storage_account.st-qliufrontend-test.primary_web_host}/public/index.html"
-        }
+#         target_url {
+#           value = "https://${azurerm_storage_account.st-qliufrontend-test.primary_web_host}/public/index.html"
+#         }
 
-        preserve_query_string = false
-      }
-    }
-  }
-}
+#         preserve_query_string = false
+#       }
+#     }
+#   }
+# }
 
 # resource "time_sleep" "wait_60_seconds" {
 #   create_duration = "300s" # Wait for 60 seconds
